8a331091717d4eb3c16f8ba7d47d82ead5fe324c
killua8p <killua8p@gmail.com>
2013-07-09 23:14:03 +1000

    Add RAPerson

---
+++ RemoteAlarm/RAPerson.h
@@ -0,0 +1,19 @@
+//
+//  RAPerson.h
+//  RemoteAlarm
+//
+//  Created by Alan Huang on 9/07/13.
+//  Copyright (c) 2013 Alan Huang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RAPerson : NSObject
+
+@property NSInteger recordID;
+@property (nonatomic, strong) NSString *firstName;
+@property (nonatomic, strong) NSString *lastName;
+@property (nonatomic, strong) NSArray *tel;
+@property (nonatomic, strong) NSArray *email;
+
+@end
---
+++ RemoteAlarm/RAPerson.m
@@ -0,0 +1,15 @@
+//
+//  RAPerson.m
+//  RemoteAlarm
+//
+//  Created by Alan Huang on 9/07/13.
+//  Copyright (c) 2013 Alan Huang. All rights reserved.
+//
+
+#import "RAPerson.h"
+
+@implementation RAPerson
+
+@synthesize recordID, firstName, lastName, tel, email;
+
+@end
